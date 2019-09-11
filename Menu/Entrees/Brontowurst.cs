2470abc8118c04273bc147f4f971a1a908dcb866
Alex Eckstein <alexceckstein@gmail.com>
2019-09-11 14:55:55 -0500

    Add files via upload

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DinoDiner.Menu.Entree
+{
+    public class Brontowurst
+    {
+        public Brontowurst()
+        {
+            private List<string> ingredients;
+            private double price;
+            private unit calories;
+            public double Price
+            {
+                get { return price; }
+                set { price = 5.36; }
+            }
+
+            public List<string> Ingredients
+            {
+                get { return ingredients; }
+                set { ingredients =  ["brautwurst", "whole-wheat bun", "peppers", "onions"]; }
+            }
+            public unit Calories
+            {
+                get { return calories; }
+                set { calories = 498; }
+            }
+        }
+
+    public void Bun();
+    public void Pepper();
+    public void onions();
+
+    }
+}
