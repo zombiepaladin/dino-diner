694637ea3af1f8537b230528a5633e98f76c57f0
Alex Eckstein <alexceckstein@gmail.com>
2019-09-11 20:13:34 -0500

    Finished Brontowurst

--- Menu/Entrees/Brontowurst.cs
+++ Menu/Entrees/Brontowurst.cs
@@ -1,39 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace DinoDiner.Menu.Entree
+namespace DinoDiner.Menu.Entrees
 {
     public class Brontowurst
     {
-        private List<string> ingredients = new List<string>();
+        private List<string> ingredients = new List<string>() { "brautwurst", "whole-wheat bun", "peppers", "onions" };
         private double price;
-        private unit calories;
+        private uint calories;
+        private bool bun = true;
+        private bool peppers = true;
+        private bool onions = true;
         public double Price
         {
             get { return price; }
             set { price = value; }
         }
-
         public List<string> Ingredients
         {
             get { return ingredients; }
             set { ingredients = value; }
         }
-        public unit Calories
+        public uint Calories
         {
             get { return calories; }
             set { calories = value; }
         }
-
+        /// <summary>
+        /// Constructor of the class, adds ingredients to list if not held
+        /// </summary>
         public Brontowurst()
         {
             this.Calories = 498;
             this.Price = 5.36;
-            this.Ingredients = new List<string>("brautwurst", "whole-wheat bun", "peppers", "onions");
+            
+            if (bun) ingredients.Add("whole-wheat bun");
+            if (peppers) ingredients.Add("pepers");
+            if (onions) ingredients.Add("onions");
+            this.Ingredients = ingredients;
+        }
+        public void HoldBun()
+        {
+            this.bun = false;
         }
 
-    public void Bun();
-    public void Pepper();
-    public void onions();
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
 
+        public void HoldOnion()
+        {
+            this.onions = false;
+        }
     }
 }
---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenuTest.Entrees
+{
+    public class DinoNuggets
+    {
+        private double price = ;
+        private uint calories;
+        private List<string> ingredients = new List<string>()
+        {
+            "Chicken Nugget", "Chicken Nugget",
+            "Chicken Nugget", "Chicken Nugget",
+            "Chicken Nugget"
+        };
+        public DinoNuggets()
+        {
+
+        }
+
+        public uint Calories { get; set; }
+        public List<string> Ingredients { get; internal set; }
+        internal void AddNugget()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
