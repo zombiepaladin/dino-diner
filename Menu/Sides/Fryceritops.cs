6cbba9e33f1c8782d346bd799497740a66d4664b
amberry <amberry@F1092-30-2118>
2019-09-18 15:22:23 -0500

    Code to fix from class

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class Fryceritops : Side
+    {
+
+        public  Size size;
+        public override Size()
+        {
+
+            set {
+                size = value;
+                switch (size)
+                {
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                    case Size.Medium:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                    case Size.Small:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                }
+            }
+
+
+
+
+        }
+
+
+        public Fryceritops()
+        {
+
+            /*Case(this.Size){
+
+            }*/
+            this.Price = 0.99;
+            this.Calories = 222;
+            this.Ingredients = new List<string>() { "Potato", "Salt", "Vegtable Oil" };
+        }
+        
+       
+
+    }
+}
--- Menu/Sides/Side.cs
+++ Menu/Sides/Side.cs
@@ -27,12 +27,12 @@ namespace DinoDiner.Menu.Sides
         /// <summary>
         /// Gets the ingredients list
         /// </summary>
-        public List<string> Ingredients { get; }
+        public  List<string> Ingredients { get; protected set; }
 
         /// <summary>
         /// Gets or sets the size
         /// </summary>
-        public Size Size { get; set; }
+        public abstract Size Size { get; set; }
 
     }
 }
