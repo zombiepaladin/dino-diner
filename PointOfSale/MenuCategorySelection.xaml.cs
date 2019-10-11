43aeb2878efa8478c044720e0755dafcb567e086
Thomas Paul <tomango8@users.campus>
2019-10-11 14:57:09 -0500

    updated

---
+++ PointOfSale/MenuCategorySelection.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for MenuCategorySelection.xaml
+    /// </summary>
+    public partial class MenuCategorySelection : Page
+    {
+        public MenuCategorySelection()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ComboSelection());
+        }
+
+
+    }
+}
